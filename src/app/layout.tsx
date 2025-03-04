85d0d0077c9e424e37a0c7ff778d22d7a84edf2c
sahil-sahu <sahilku2003@gmail.com>
2025-03-04 15:36:08 +0530

    add speed insights of vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Public_Sans } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const publicSans = Public_Sans({
   variable: "--font-public-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`${publicSans.className} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
